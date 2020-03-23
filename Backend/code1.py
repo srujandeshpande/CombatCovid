bb8554da8e5b1e2cb7a88ea20e316d97df1ecb16
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-03-23 12:08:27 +0530

    Code1 contains pymongo implementations

--- Backend/code1.py
+++ Backend/code1.py
@@ -7,13 +7,12 @@ app = Flask(__name__)
 
 mongo = pymongo.MongoClient('mongodb+srv://srujandeshpande:mongodb@cluster0-e0fen.azure.mongodb.net/test?retryWrites=true&w=majority', maxPoolSize=50, connect=True)
 
-db = pymongo.database.Database(mongo, 'covid_v1')
-col = pymongo.collection.Collection(db, 'UserData')
-col_results = json.loads(dumps(col.find()))
-
 @app.route("/aaa")
 def home_page():
-	return(str(col_results))
+    db = pymongo.database.Database(mongo, 'covid_v1')
+    col = pymongo.collection.Collection(db, 'UserData')
+    col_results = json.loads(dumps(col.find()))
+    return(str(col_results))
 
 @app.route('/abc')
 def hello_world():
--- Backend/code2.py
+++ Backend/code2.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_pymongo import PyMongo
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb+srv://srujandeshpande:mongodb@cluster0-e0fen.azure.mongodb.net/test?retryWrites=true&w=majority'
-mongo = PyMongo(app)
+app.config['MONGO_URI'] = 'mongodb+srv://srujandeshpande:mongodb@cluster0-e0fen.azure.mongodb.net/covid_v1?retryWrites=true&w=majority'
+mongo = PyMongo(app, connect=True)
 
 @app.route("/")
 def home_page():
