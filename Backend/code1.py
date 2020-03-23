f85a17d17c5ec56fe4bf7fd33f07f71f6186e3fc
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-03-23 13:37:05 +0530

    POST requests working, adding to atlas

--- Backend/code1.py
+++ Backend/code1.py
@@ -2,18 +2,24 @@ import pymongo
 from bson.json_util import dumps
 import json
 from flask import Flask
+from flask import request
 #from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 mongo = pymongo.MongoClient('mongodb+srv://srujandeshpande:mongodb@cluster0-e0fen.azure.mongodb.net/test?retryWrites=true&w=majority', maxPoolSize=50, connect=True)
+db = pymongo.database.Database(mongo, 'covid_v1')
 
-@app.route("/aaa")
-def home_page():
-    db = pymongo.database.Database(mongo, 'covid_v1')
-    col = pymongo.collection.Collection(db, 'UserData')
-    col_results = json.loads(dumps(col.find()))
-    return(str(col_results))
+@app.route("/add_new_user", methods=['POST'])
+def add_new_user():
+    userData = pymongo.collection.Collection(db, 'User_Data')
+    searchword = request.json
+    objid = userData.insert_one(searchword).inserted_id
+    #searchword = request.form['uname']
+    #searchword = request.args.get('uname')
+    return str(objid)
 
 @app.route('/abc')
 def hello_world():
-	return 'Hello, World!'
+    col = pymongo.collection.Collection(db, 'User_Data')
+    col_results = json.loads(dumps(col.find()))
+    return(str(col_results))
