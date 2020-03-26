a6ed5507ddaab1f3587787c6bc963706eac78c61
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-03-26 17:23:48 +0530

    Added personalised add new ema user

--- static/js/add_new_user1.js
+++ static/js/add_new_user1.js
@@ -4,8 +4,37 @@ function new_admin() {
   role = document.getElementById('new_ema_role')
   role.setAttribute("value","admin")
   chc = document.getElementById('new_ema_chc')
-  chc.style.visibility='hidden'
+  chc.style.display='none'
   phc = document.getElementById('new_ema_phc')
-  phc.style.visibility='hidden'
-  console.log("ppppppppppppppppppp")
+  phc.style.display='none'
+}
+function new_chc() {
+  form_div = document.getElementById('form_add_ema_user')
+  form_div.style.visibility='visible'
+  role = document.getElementById('new_ema_role')
+  role.setAttribute("value","chc")
+  chc = document.getElementById('new_ema_chc')
+  chc.style.display='none'
+  phc = document.getElementById('new_ema_phc')
+  phc.style.display='none'
+}
+function new_phc() {
+  form_div = document.getElementById('form_add_ema_user')
+  form_div.style.visibility='visible'
+  role = document.getElementById('new_ema_role')
+  role.setAttribute("value","phc")
+  chc = document.getElementById('new_ema_chc')
+  chc.style.display='inline'
+  phc = document.getElementById('new_ema_phc')
+  phc.style.display='none'
+}
+function new_mo() {
+  form_div = document.getElementById('form_add_ema_user')
+  form_div.style.visibility='visible'
+  role = document.getElementById('new_ema_role')
+  role.setAttribute("value","mo")
+  chc = document.getElementById('new_ema_chc')
+  chc.style.display='inline'
+  phc = document.getElementById('new_ema_phc')
+  phc.style.display='inline'
 }
