b615b84cb64fe9886a25ac51523074f97947fe01
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-03-29 02:29:35 +0530

    click more

--- static/js/admin_dashboard_new.js
+++ static/js/admin_dashboard_new.js
@@ -12,12 +12,12 @@ $(function(){
     var count = msg['count']
     var r = "record"
     for (var i=0;i<count;i++){
-          $("#userDataList").append('<tr><td>'+(i+1)+'</td><td>'+msg[r+i]['phone_number']+'</td><td>'+msg[r+i]['date_time_quarantined']+'</td><td>'+msg[r+i]['first_name']+'</td><td>'+msg[r+i]['last_name']+'</td><td>'+msg[r+i]['dob']+'</td><td>'+msg[r+i]['currently_under_quarantine']+'</td><td>'+msg[r+i]['email']+'</td><td>'+msg[r+i]['mo_phone_number']+'</td></tr>');
+          $("#userDataList").append('<tr><td>'+(i+1)+'</td><td id="userph">'+msg[r+i]['phone_number']+'</td><td>'+msg[r+i]['date_time_quarantined']+'</td><td>'+msg[r+i]['first_name']+'</td><td>'+msg[r+i]['last_name']+'</td><td>'+msg[r+i]['dob']+'</td><td>'+msg[r+i]['currently_under_quarantine']+'</td><td>'+msg[r+i]['email']+'</td><td>'+msg[r+i]['mo_phone_number']+'</td></tr>');
         }
         $("#userDataListHeader").html("User Data")
-        $('#induser').click(function() {
-          //var phno = $(this).data("phno")
-          alert("phno");
+        $('#userph').click(function(event) {
+          phno = event.target.innerHTML
+          alert(phno)
         });
     }
   });
@@ -33,15 +33,13 @@ $(function(){
     var count = msg['count']
     var r = "record"
     for (var i=0;i<count;i++){
-      $("#userTempList").append('<tr><td>'+(i+1)+'</td><td>'+msg[r+i]['phone_number']+'</td><td>'+msg[r+i]['temperature']+'</td><td>'+msg[r+i]['Date-time']+'</td></tr>');
+      $("#userTempList").append('<tr><td>'+(i+1)+'</td><td id="userph">'+msg[r+i]['phone_number']+'</td><td>'+msg[r+i]['temperature']+'</td><td>'+msg[r+i]['Date-time']+'</td></tr>');
     }
     $("#userTempListHeader").html("Temperature Data")
-        /*
-        $('#induser').click(function() {
-          //var phno = $(this).data("phno")
-          alert("phno");
-        });
-        */
+    $('#userph').click(function(event) {
+      phno = event.target.innerHTML
+      alert(phno)
+    });
     }
   });
 
@@ -56,10 +54,14 @@ $(function(){
     var count = msg['count']
     var r = "record"
     for (var i=0;i<count;i++){
-      $("#userCCList").append('<tr><td>'+(i+1)+'</td><td>'+msg[r+i]['phone_number']+'</td><td>'+msg[r+i]['Date-time']+'</td><td>'+msg[r+i]['contact-pno']+'</td><td>'+msg[r+i]['contact-name']+'</td><td>'+msg[r+i]['contact-address']+'</td></tr>');
+      $("#userCCList").append('<tr><td>'+(i+1)+'</td><td id="userph">'+msg[r+i]['phone_number']+'</td><td>'+msg[r+i]['Date-time']+'</td><td>'+msg[r+i]['contact-pno']+'</td><td>'+msg[r+i]['contact-name']+'</td><td>'+msg[r+i]['contact-address']+'</td></tr>');
     }
     $("#userCCListHeader").html("Close Contact Data")
     }
+    $('#userph').click(function(event) {
+      phno = event.target.innerHTML
+      alert(phno)
+    });
   });
 
   $.ajax({
@@ -73,10 +75,14 @@ $(function(){
     var count = msg['count']
     var r = "record"
     for (var i=0;i<count;i++){
-      $("#userDistressList").append('<tr><td>'+(i+1)+'</td><td>'+msg[r+i]['phone_number']+'</td><td>'+msg[r+i]['Date-time']+'</td><td>'+msg[r+i]['active']+'</td></tr>');
+      $("#userDistressList").append('<tr><td>'+(i+1)+'</td><td id="userph">'+msg[r+i]['phone_number']+'</td><td>'+msg[r+i]['Date-time']+'</td><td>'+msg[r+i]['active']+'</td></tr>');
     }
     $("#userDistressListHeader").html("Distress Data")
     }
+    $('#userph').click(function(event) {
+      phno = event.target.innerHTML
+      alert(phno)
+    });
   });
 
 
